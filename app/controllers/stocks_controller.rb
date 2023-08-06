241186fd17d0503e849ad493172826d831aaab5f
1682123 <123kato.izumi@gmail.com>
2023-08-06 15:56:20 +0900

    編集機能と削除機能の実装

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,5 @@
 class StocksController < ApplicationController
+  before_action :set_stock, only: [:edit, :update]
   
   def index
     @stocks = Stock.includes(:user).order("expiration_date ASC")
@@ -31,19 +32,43 @@ class StocksController < ApplicationController
   end
 
   def edit
-    
+    stock_attributes = @stock.attributes
+    @stock_form = StockForm.new(stock_attributes)
+
+    #複数タグの表示
+    @stock_form.tag_name = @stock.tags.pluck(:tag_name).join(',')
   end
 
   def update
-    
+    @stock_form = StockForm.new(stock_form_params)
+
+    # 画像を選択し直していない場合は、既存の画像をセットする
+    @stock_form.image ||= @stock.image.blob
+
+    # タグの複数保存
+    tag_list = params[:stock_form][:tag_name].split(",")
+
+    if @stock_form.valid?
+      @stock_form.update(stock_form_params, @stock, tag_list)
+      redirect_to stocks_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-    
+    stock = Stock.find(params[:id])
+    if stock.destroy
+      redirect_to stocks_path
+    end
   end
 
   private
   def stock_form_params
     params.require(:stock_form).permit(:food_name, :expiration_date, :memo, :tag_name, :image).merge(user_id: current_user.id)
+  end
+
+  def set_stock
+    @stock = Stock.find(params[:id])
   end
 end
--- app/models/stock_form.rb
+++ app/models/stock_form.rb
@@ -26,4 +26,21 @@ class StockForm
     end
   end
 
+  def update(params, stock, tag_list)
+    #一度タグの紐付けを消す
+    stock.stock_tags.destroy_all
+
+    #paramsの中のタグの情報を削除。同時に、返り値としてタグの情報を変数に代入
+    tag_name = params.delete(:tag_name)
+
+    #もしタグの情報がすでに保存されていればインスタンスを取得、無ければインスタンスを新規作成
+    tag_list.each do |tag_name|
+      tag = Tag.where(tag_name: tag_name).first_or_initialize
+      # タグを保存
+      tag.save
+      StockTag.create(stock_id: stock.id, tag_id: tag.id)
+      stock.update(params)
+    end
+  end
+
 end
