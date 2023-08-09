ecd32bf78e5623d95b5fa01e7375193a3667416b
1682123 <123kato.izumi@gmail.com>
2023-08-09 16:22:25 +0900

    買い物リスト投稿機能の実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,4 +3,35 @@ class MemosController < ApplicationController
     # ログインユーザーの投稿のみの表示
     @user_memos = current_user.memos.order("created_at DESC")
   end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.create(memo_params)
+    if @memo.save
+      redirect_to memos_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def memo_params
+    params.require(:memo).permit(:title, :memo).merge(user_id: current_user.id)
+  end
+
 end
