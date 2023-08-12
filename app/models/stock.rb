d2bb44eac67c67eab6ca742a10769101afe0cc49
1682123 <123kato.izumi@gmail.com>
2023-08-12 14:43:31 +0900

    モデルに設定

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,4 +7,12 @@ class Stock < ApplicationRecord
   has_many :stock_tags, dependent: :destroy
   has_many :tags, through: :stock_tags
   has_one_attached :image
+
+  def self.search(search)
+    if search != ""
+      Stock.where('food_name LIKE(?)', "%#{search}%")
+    else
+      @user_stocks = current_user.stocks.order("expiration_date ASC")
+    end
+  end
 end
